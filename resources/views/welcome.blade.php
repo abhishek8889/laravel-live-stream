8003dfeb2dfeaab03c35572111678d9bcd14bb17
abhishek sharma <sharmaabhi8889@gmail.com>
2023-01-17 19:36:32 +0530

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('front_layout.master')
 @section('content')
-test 
+
 <div class="container border card " style="margin-top:100px;">
 @if(isset(auth()->user()->name))
     <div style="padding:50px;">
@@ -12,4 +12,18 @@ test
     <a href="{{ route('login') }}"> Go for login -> </a>
 @endif
 </div>
+
+<div class="container card"> 
+    <div class="p-5">
+        <h3> card details</h3> 
+        <div class="pt-5" id="card-element"></div>
+    </div>
+</div>
+<script src="https://js.stripe.com/v3/"></script>
+<script>
+    const stripe = Stripe('{{ env('STRIPE_PUB_KEY') }}');
+    const elements = stripe.elements();
+    const cardElement = elements.create('card');
+    cardElement.mount('#card-element');
+</script>
 @endsection
